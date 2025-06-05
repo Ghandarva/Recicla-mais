8630664c6800791f4cbacb877cc05ebe2a675069
Ghandarva <gigiosiracs@gmail.com>
2025-06-05 10:34:01 -0300

    Arquivo bat para auxiliar nas atualizações do git

--- GitPull.bat
+++ GitPull.bat
@@ -14,7 +14,7 @@ echo == Adicionando arquivos ==
 git add .
 
 echo == Commitando arquivos ==
-git commit -m "Atualizaçao"
+git commit -m "Update"
 
 echo == Enviando para o GitHub ==
 git push origin main
